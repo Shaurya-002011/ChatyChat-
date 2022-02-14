2b6a4f857e9ad2aeee361026b6d2e36460189225
CinosBoss <85790104+Shaurya-002011@users.noreply.github.com>
2022-02-14 18:29:41 +0530

    Add files via upload

---
+++ chatyChat_student_login.js
@@ -0,0 +1,9 @@
+function addUser()
+{
+    student_name = document.getElementById("student_name").value;
+    localStorage.setItem("student_name", student_name);
+    window.location = "chatyChat_room.html";
+}
+
+student_name = localStorage.getItem("student_name");
+document.getElementById("user_name").innerHTML="Welcome "+ student_name +"!!!!!!";
